d918aef1fce1e76eef333d59f5fc239f97ff1d7e
u6013779 <89681312+u6013779@users.noreply.github.com>
2021-09-15 21:29:52 -0600

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,48 @@
+first_number = int(input('Type the first number: ')) ;\
+second_number = int(input('Type the second number: ')) ;\
+print("The sum is: ", first_number + second_number)
+
+x = 1         
+y = x + 5     
+z = y         
+
+x = 1      
+x = 1.0     
+
+x = 1
+print(type(x)) 
+
+x = 1.0
+print(type(x)) 
+
+x = True
+print(type(x)) 
+
+x = 'This is a string'
+print(x) 
+print(type(x))
+y = "This is also a string"
+
+x = "Hello" + "World!"
+print(x)
+
+print("Hello World!")
+
+PI = 3.14
+PI
+
+name = input("KiraLee:")
+print(KiraLee)
+
+print("KiraLee")
+name = input()
+print(KiraLee)
+
+x = input('2021:')
+print(type(x))
+
+x = int(input('2021:'))
+print(type(x))
+
+x = 5
+print('2021' + str(x))
